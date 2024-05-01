d4b97ae9d1ce8348e827697b7fbc36b074eddee4
cs50-romain <cs50.git.romain@gmail.com>
2024-04-30 21:53:26 -0400

    added download & inventory routes

--- api/handlers.go
+++ api/handlers.go
@@ -1,9 +1,12 @@
 package api
 
 import (
-	util "cs50-romain/tagl/utils"
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"cs50-romain/tagl/types"
+	util "cs50-romain/tagl/utils"
 )
 
 func getIndex(w http.ResponseWriter, r *http.Request) {
@@ -14,5 +17,35 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 func handleSubmit(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(util.Fore(util.Yellow, "request:"), r.RequestURI)
 	fmt.Println(r.Body)
-	http.ServeFile(w, r, "/index")
+	http.ServeFile(w, r, "./static/index.html")
+}
+
+func handleInventory(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(util.Fore(util.Yellow, "request:"), r.RequestURI)
+	item := types.NewItem("HDMI2VGA", 2)
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/json")
+	WriteJSON(w, item)
+}
+
+// User receives a csv file of his inventory. Need csv, writeto.
+func handleDownload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(util.Fore(util.Yellow, "request:"), r.RequestURI)
+	
+	// DATA WILL BE IMPORTED FROM DB LATER
+	data := [][]string{
+		{"first_name", "last_name", "username"},
+		{"Rob", "Pike", "rob"},
+		{"Ken", "Thompson", "ken"},
+		{"Robert", "Griesemer", "gri"},
+	}
+
+	if err := WriteCSV(data); err != nil {
+	        w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode("Unable to open file ")
+		return	
+	}
+	w.Header().Set("Content-Disposition", "attachment; filename=inventory.csv")
+	w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+	http.ServeFile(w, r, "./inventory.csv")
 }
--- api/server.go
+++ api/server.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"encoding/csv"
+	"encoding/json"
+	"io"
 	"net/http"
+	"os"
 )
 
 type Server struct {
@@ -15,7 +19,30 @@ func NewServer(addr string) *Server {
 }
 
 func (s *Server) Start() error {
-	http.HandleFunc("/index", getIndex)
-	http.HandleFunc("/submit", handleSubmit)
-	return http.ListenAndServe(s.addr, nil)
+	router := http.NewServeMux()
+	router.HandleFunc("/index", getIndex)
+	router.HandleFunc("/submit", handleSubmit)
+	router.HandleFunc("/inventory", handleInventory)
+	router.HandleFunc("/download", handleDownload)
+	return http.ListenAndServe(s.addr, router)
+}
+
+func WriteJSON(w io.Writer, d any) error {
+	return json.NewEncoder(w).Encode(d)
+}
+
+func WriteCSV(data [][]string) error {
+	file, err := os.OpenFile("./inventory.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	cw := csv.NewWriter(file)
+	for _, d := range data {
+		if err := cw.Write(d); err != nil {
+			return err
+		}
+	}
+	cw.Flush()
+	return nil
 }
