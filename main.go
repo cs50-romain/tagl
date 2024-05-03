0a791e2e1e1784153d645f9f3e8608ee1b727359
cs50-romain <cs50.git.romain@gmail.com>
2024-05-02 21:03:27 -0400

    init database

--- main.go
+++ main.go
@@ -7,13 +7,19 @@ import (
 
 	"cs50-romain/tagl/api"
 	"cs50-romain/tagl/utils"
+	"cs50-romain/tagl/storage"
 )
 
 func main() {
 	portPtr := flag.String("port", ":6969", "port for http server to listen")
 	flag.Parse()
 
-	s := api.NewServer(*portPtr)
+	db, err := storage.NewMysqlStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := api.NewServer(*portPtr, db)
 	fmt.Println(util.Fore(util.Cyan, "info: server running on port"), *portPtr)
 	log.Fatal(s.Start())
 }
